62ca46b3e2cee743fa268b4c4555fc35afaba2bc
Rouven Broszeit <roubro1991@gmx.de>
2021-09-02 14:41:39 +0200

    linux build still working while using some newer and official repos

--- deps/sync.sh
+++ deps/sync.sh
@@ -3,9 +3,10 @@ set -e
 echo "Sync started."
 for i in \
 https://github.com/Iunusov/libmp3lame-CMAKE.git \
-https://github.com/Iunusov/OGG-Vorbis-CMAKE.git \
+https://github.com/xiph/ogg.git \
+https://github.com/xiph/vorbis.git \
 https://github.com/Iunusov/libshout-CMAKE.git \
-https://github.com/IngwiePhoenix/FLTK.git \
+https://github.com/fltk/fltk.git \
 https://r-tur@bitbucket.org/r-tur/vst_sdk_2.4.git \
 https://github.com/xiph/flac.git
 do
